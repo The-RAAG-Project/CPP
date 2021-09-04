3bbdad700f233e2d480156d11a577479bc588c3f
Ankit Raj Mahapatra <ankitrajmahapatra@gmail.com>
2021-09-04 17:31:33 +0530

    Add files via upload

---
+++ demo.cpp
@@ -0,0 +1,24 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	// Read the image file
+	Mat image = imread("demo.png");
+	// Check for failure
+	if (image.empty())
+	{
+		cout << "Image Not Found!!!" << endl;
+		cin.get(); //wait for any key press
+		return -1;
+	}
+	// Show our image inside a window.
+	imshow("demo.png", image);
+
+	// Wait for any keystroke in the window
+	waitKey(0);
+	return 0;
+}
