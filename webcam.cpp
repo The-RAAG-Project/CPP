1f379a818d14012a864627108c0a8d99364c2fde
ayush19283 <89829211+ayush19283@users.noreply.github.com>
2021-09-28 17:54:02 +0530

    Add files via upload
    opening webcam using opencv in c++

---
+++ webcam.cpp
@@ -0,0 +1,33 @@
+ 
+#include "opencv2/opencv.hpp"
+#include "iostream"
+
+int main(int, char**) {
+	// open the first webcam plugged in the computer
+	cv::VideoCapture camera(0);
+	if (!camera.isOpened()) {
+		std::cerr << "ERROR: Could not open camera" << std::endl;
+		return 1;
+	}
+
+	// create a window to display the images from the webcam
+	cv::Mat frame;
+	camera >> frame;
+	// this will contain the image from the webcam
+	
+
+	// display the frame until you press a key
+	while (camera.read(frame)) {
+		 
+		//std::cout << "**";
+
+		// capture the next frame from the webcam
+		
+		// show the image on the window
+		cv::imshow("Webcam", frame);
+		// wait (10ms) for a key to be pressed
+		if (cv::waitKey(16) >= 0)
+			continue;
+	}
+	return 0;
+}
